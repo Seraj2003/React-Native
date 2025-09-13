c2518560b4d5ef30c6b4ce3e4bfc8bb9f8465dc4
Seraj2003 <mdseraj002020@gmail.com>
2025-09-13 21:09:49 +0530

    sdtfyguhhfg

---
+++ scr/Put_Api.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Put_Api = () => {
+  return (
+    <View>
+      <Text>Put_Api</Text>
+    </View>
+  )
+}
+
+export default Put_Api
